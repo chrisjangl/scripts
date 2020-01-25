6a10cbb500097df05ee7684401486db331816967
Chris Jangl <chris@digitallycultured.com>
2020-01-25 14:21:48 -0500

    create script to run all updates for a given site

--- hosting_reports/run_updates.sh
+++ hosting_reports/run_updates.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # update plugins, record in git and report
 # v1.0
 
@@ -13,41 +14,29 @@ DIRECTORY=$1
 # let's get started
 clear
 
-echo "We'll be performing updates in $DIRECTORY here. Let's get started. "
+echo "We'll be performing updates for WordPress core, active plugins & the parent theme in $DIRECTORY here. Let's get started. "
 echo
 echo "-----------------------------------------------------------"
 
 # pass off to core.sh
-
-
-# echo "Here is a list of the directories we have availabe to work with: "
-# echo
-# echo "-----------------------------------------------------------"
-
-# list the directories available and ask which to work on
-cd $SERVER
-# ls
-# echo "-----------------------------------------------------------"
-
-# echo
-# read -p "REMINDER: We are about to work on $SERVER - which directory are we updating up right now? " -e SITEFOLDER
-
-# cd to that folder
-cd $DIRECTORY
-
-
-# update active plugins
-echo "Passing off to update plugins..."
+echo "Passing off to core.sh....."
 echo "..."
-update_plugins
+bash core.sh $DIRECTORY
 
-# add list of skipped plugins to reports
-echo "Passing off to report the plugins we skipped..."
+# pass off to plugins.sh
+echo "-----------------------------------------------------------"
+echo "Passing off to plugins.sh"
 echo "..."
-skipped_plugins
+bash plugins.sh $DIRECTORY
+
+# pass off to theme.sh
+echo "-----------------------------------------------------------"
+echo "Passing off to theme.sh"
+echo "..."
+bash theme.sh $DIRECTORY
 
 # close up shop
 echo "-----------------------------------------------------------"
-echo "We just attempted to update the active plugins in" $DIRECTORY
+echo "We just attempted to update WordPress core, active plugins & the parent theme in $DIRECTOR"
 echo "We ran through each step. No guarantees as to success, and there have been no fail-safes."
 echo "Adios."
