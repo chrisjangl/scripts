b3d4c688194732c8ede49c2c41d3e52f2d9ea86b
Chris Jangl <chris@digitallycultured.com>
2021-06-16 12:40:05 -0400

    make notice a bit clearer on working directory check

--- hosting_reports/is_working_directory_clean.sh
+++ hosting_reports/is_working_directory_clean.sh
@@ -1,10 +1,14 @@
 # bash
 ## checks if the working directory is clean; i.e. nothing staged, nothing changed
 
+echo "----------"
 if [ -z "$(git status --porcelain)" ]; then 
-  # Working directory clean excluding untracked files
-  echo "Working Directory is CLEAN"
+    # Working directory clean excluding untracked files
+    echo $(pwd)": "
+    echo "Working Directory is CLEAN            ++"
 else 
-  # Uncommitted changes in tracked files
-  echo "nah, we're going through chhaaannnggeessss"
+    # Uncommitted changes in tracked files
+    echo $(pwd)": "
+    echo "we're going through chhannnggessss    --"
 fi
+echo "----------"
