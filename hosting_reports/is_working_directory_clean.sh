a76485f8d3792e9dfc3c147c08f2b2598410f3f3
Chris Jangl <chris@digitallycultured.com>
2021-06-16 12:49:41 -0400

    Add todos to Working Directory script

--- hosting_reports/is_working_directory_clean.sh
+++ hosting_reports/is_working_directory_clean.sh
@@ -1,10 +1,13 @@
 # bash
 ## checks if the working directory is clean; i.e. nothing staged, nothing changed
+## @TODO: this doesn't check for any updates from remote repos
 
 echo "----------"
+git fetch
 if [ -z "$(git status --porcelain)" ]; then 
     # Working directory clean excluding untracked files
     echo $(pwd)": "
+    echo "Branch: "$(git branch --show-current)
     echo "Working Directory is CLEAN            ++"
 else 
     # Uncommitted changes in tracked files
